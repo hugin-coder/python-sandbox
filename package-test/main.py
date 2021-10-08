4f5a91a1180eb30fe8010021095307ede61e4d59
Erling Egholm <erling@elektron.fo>
2021-10-08 20:30:58 +0100

    Add package test

---
+++ package-test/main.py
@@ -0,0 +1,9 @@
+from pkg1 import mod1
+from pkg1.pkg2a import mod1 as am1
+from pkg1.pkg2a import mod2 as am2
+from pkg1.pkg2b import mod1 as bm1
+
+mod1.say()
+am1.say()
+am2.say()
+bm1.say()
---
+++ package-test/pkg1/pkg2b/mod1.py
@@ -0,0 +1,3 @@
+def say():
+    print(f'Hi from {__name__}')
+    
